e21902db690c2e283d1392c5e8fbfdf6e197a91b
Graham Barker <graham.barker@donson.co.uk>
2020-06-22 11:16:46 +0100

    Dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  entry: ["webpack/hot/dev-server", "./app.js"],
+  entry: ['webpack/hot/dev-server', './app.js'],
 
   output: {
-    filename: "app.js",
-    path: __dirname + "/build",
+    filename: 'app.js',
+    path: __dirname + '/build',
   },
 
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /.js?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
-        }
+          presets: ['es2015', 'react'],
+        },
       },
       {
         test: /\.scss$/,
-        loader: "style-loader!css-loader!sass-loader"
-      }
-    ]
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.jsx', '.css', '.scss']
-  }
+    extensions: ['', '.js', '.json', '.jsx', '.css', '.scss'],
+  },
 }
